2a5e17bef58a5f2aa0c0439b4d9192d405d71ed7
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2023-09-24 01:41:19 -0400

    new

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//listen for request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Yoshi found an egg'},
+        {title: 'Mario finds stars', snippet: 'Mario found some stars'},
+        {title: 'How to defeat Bowser', snippet: 'Tricks to beat Bowser'}
+    ];
+    res.render('index', {title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<p>about page</p>');
+    res.render('about', { title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create new Blog'});
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404'});
+});
