c98fab0711fb229776c9b580f7366a5fdb2c8726
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2023-09-28 23:09:09 -0400

    new

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+//connect to database on mongo
+const dbURI = 'mongodb+srv://cscholl16:Trotty623@mongoscholl.kszprkq.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs');
-
-//listen for request
-app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Yoshi found an egg'},
-        {title: 'Mario finds stars', snippet: 'Mario found some stars'},
-        {title: 'How to defeat Bowser', snippet: 'Tricks to beat Bowser'}
-    ];
-    res.render('index', {title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
     res.render('about', { title: 'About'});
 });
+
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create new Blog'});
